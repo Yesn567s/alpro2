6495cd0b7217ef592e028bd112d30113419fee0d
Yesn't <reynardlee0510@gmail.com>
2025-02-21 10:33:30 +0700

    Update main.cpp

--- Alpro Week 2/main.cpp
+++ Alpro Week 2/main.cpp
@@ -35,13 +35,13 @@ void timer_end(){
     }
 }
 
-void create_file(){ // belom di random
+void create_file(){
     srand(time(0));
     int random,sizes=1000000;
     unordered_set <int> isi; // unordered set can only store unique numbers (you know what that means :grins:)
 
     while (isi.size() < sizes){
-        random = rand() % 2 000000 + 1;
+        random = rand() % 2000000 + 1;
         isi.insert(random);
     }
 
@@ -54,12 +54,37 @@ void create_file(){ // belom di random
     outf.close();
 }
 
-void insertion_sort(){
-    cout << "Hello World!";
+void numbers_sort(){
+    fstream read_file("keluaran.txt");
+    vector <int> temp;
+
+    while ((read_file >> output)){
+        temp.push_back(output); //read le file and insert to vector
+    }
+    read_file.close();
+
+    timer_start();
+    cout << "Sorting numbers...";
+    sort(temp.begin(), temp.end()); // pake sort algorithm biar cepet
+
+    system("cls");
+    timer_end();
+    cout << "Sorting complete" << endl;
+
+    outf.open("keluaran_sort.txt",ios::out); // create the file as "keluaran.txt"
+
+    for (int i=0; i<temp.size(); i++){
+        outf << temp[i] << endl;
+    }
+
+    outf.close();
+
+    cout << "\nPress any key to return to the menu...";
+    _getch();
 }
 
 void linear_search(){
-    fstream read_file("keluaran.txt");
+    fstream read_file("keluaran_sort.txt");
     vector <int> temp;
     bool found = false;
     int i, target;
@@ -85,9 +110,8 @@ void linear_search(){
         }
     }
 
-    timer_end();
-
     system("cls");
+    timer_end();
 
     if (found == true){
         cout << "Number found, Index: " << i << endl;
@@ -104,56 +128,7 @@ void binary_search(){
 }
 
 void interpolation_search(){
-    fstream read_file("keluaran.txt");
-    vector <int> temp;
-    bool found = false;
-    int i, target;
-
-    while ((read_file >> output)){
-        temp.push_back(output); //read le file and insert to vector
-    }
-    read_file.close();
-
-    int n=temp.size();
-
-    cout << "Enter number you want to find: " << endl << ">> "; cin >> target;
-
-    system("cls");
-    cout << "Searching...";
-
-    timer_start();
-    int low = 0, high = n-1,mid;
-    while(true){
-        if(high>=low||temp(low)<=target||temp(high)>=target){
-            break;
-        }
-        else{
-            mid = low + (target-temp(low)/(temp(high)-temp(low))*(high-low))/1;
-            if(temp(mid)==target){
-                found = true;
-                break;
-            }
-            else if(temp(mid)<target){
-                low = mid + 1;
-            }
-            else if(temp(mid)>target){
-                high = mid - 1;
-            }
-        }
-    }
-
-    timer_end();
-
-    system("cls");
-
-    if (found == true){
-        cout << "Number found, Index: " << i << endl;
-    } else {
-        cout << "Index not found" << endl;
-    }
-
-    cout << "\nPress any key to return to the menu...";
-    _getch();
+    cout << "Hello World!";
 }
 
 int main()
@@ -181,7 +156,7 @@ int main()
             system("cls");
             time_state = 2;
             cout << "=== Pick a manipulation method ===" << endl;
-            cout << "1. Insertion sort" << endl;
+            cout << "1. Sort numbers" << endl;
             cout << "2. Linear search" << endl;
             cout << "3. Binary seacrh" << endl;
             cout << "4. Interpolation search" << endl;
@@ -191,7 +166,7 @@ int main()
             switch (submenu_input){
             case 1:
                 system("cls");
-                insertion_sort();
+                numbers_sort();
                 break;
             case 2:
                 system("cls");
